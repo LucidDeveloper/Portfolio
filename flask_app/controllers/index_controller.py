4b6c1e866d48aa366ea562649b95bbf7c75fd399
LucidDeveloper <gianni.javier@luciddeveloper.com>
2025-05-12 16:17:38 -0400

    Added HTML files: * flask_app/templates/about.html * flask_app/templates/contact.html * flask_app/templates/payments.html
    Updated existing files to Incorporate new HTML files:
    * flask_app/controllers/index_controller.py
    * flask_app/templates/layouts/base.html

--- flask_app/controllers/index_controller.py
+++ flask_app/controllers/index_controller.py
@@ -18,6 +18,18 @@ def resume():
 def projects():
     return render_template('/projects.html')
 
+@app.route('/payments')
+def payments():
+    return render_template('/payments.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('/contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('/about.html')
+
 @app.route('/home')
 def home():
     return redirect('/')
