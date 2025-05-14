0dd19c0255452f2eee79b4b06b847ee3409b0015
LucidDeveloper <gianni.javier@luciddeveloper.com>
2025-05-14 18:00:27 -0400

    Added New Test Case for Payment modified:flask_app/controllers/index_controller.py modified:   flask_app/templates/payments.html

--- flask_app/controllers/index_controller.py
+++ flask_app/controllers/index_controller.py
@@ -20,9 +20,11 @@ def projects():
 
 @app.route('/payments')
 def payments():
+    TEST1_PRICING_TABLE_ID = os.environ.get("TEST1_PRICING_TABLE_ID")
+    TEST1_PUBLISHABLE_KEY = os.environ.get("TEST1_PUBLISHABLE_KEY")
     PRICING_TABLE_ID = os.environ.get("PRICING_TABLE_ID")
     PUBLISHABLE_KEY= os.environ.get("PUBLISHABLE_KEY")
-    return render_template('/payments.html',PRICING_TABLE_ID=PRICING_TABLE_ID, PUBLISHABLE_KEY=PUBLISHABLE_KEY)
+    return render_template('/payments.html', TEST1_PRICING_TABLE_ID=TEST1_PRICING_TABLE_ID, TEST1_PUBLISHABLE_KEY=TEST1_PUBLISHABLE_KEY,PRICING_TABLE_ID=PRICING_TABLE_ID, PUBLISHABLE_KEY=PUBLISHABLE_KEY)
 
 @app.route('/contact')
 def contact():
